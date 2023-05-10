13dca60d5d9d4bd52643de006a42ad6fbd5bf8da
libradito <47046453+libradito@users.noreply.github.com>
2023-05-09 19:33:49 -0700

    getStaticProps function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,33 @@
-export default function Home() {
+import { useRouter } from 'next/router'
+import { Button } from '@mui/material'
+import styles from '../styles/Home.module.css'
+
+export default function Home({name, summary}) {
+  const router = useRouter();
   return (
-    <>
-     <h1>Home Page</h1>
-    </>
+    <section className={styles.Home}>
+      <h1 className={styles.Name}>{name}</h1>
+      <div className={styles.Summary}>{summary}</div>
+      <div>
+        <Button 
+          variant="contained" 
+          size='large' 
+          onClick={() => router.push("/projects")}  
+          >
+          Projects
+        </Button>
+      </div>
+    </section>
   )
+}
+
+//this data will be collected at build time
+export async function getStaticProps() {
+  //Get your data from an API, CMS, etc.
+  return {
+    props: {
+      name: 'Librado',
+      "summary": "I'm a software developer."
+    }
+  }
 }
