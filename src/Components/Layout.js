08be9ad9489f1024f814103b234aea8d51ffddbf
libradito <47046453+libradito@users.noreply.github.com>
2023-05-09 19:12:32 -0700

    layout component

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,11 +1,17 @@
 import { Container } from "@mui/material";
 import Header from "./Header";
 import Footer from "./Footer";
+import Link from "next/link";
 
 export default function Layout({ children }) {
     return (
         <>
-            <Header title="Librado"></Header>
+            <Header title="Librado">
+                <Link href="/">Home</Link>
+                <Link href="/about"> About</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/contact">Contact</Link>
+            </Header>
             <Container fixed>
                 <main>{children}</main>
             </Container>
